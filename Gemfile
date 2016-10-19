0bd7f83dc8dddb56083913530a0fea2691e6b178
cgulli <caitlin.gulliford@gmail.com>
2016-10-19 16:35:31 +0100

    getting the thing to deploy, have specified database

--- Gemfile
+++ Gemfile
@@ -3,8 +3,12 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "rspec-sinatra"
-gem "capybara"
 gem "data_mapper"
 gem "dm-postgres-adapter"
 gem "dm-migrations"
 gem "database_cleaner"
+
+group :test do
+  gem 'capybara'
+  gem 'rspec'
+end
--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'sinatra/base'
 require_relative 'models/link'
-#random comment to test heroku stuff and git stuff because it is driving us mad sakldf;odsf'isdjfl;uhsdf
+
 class Bookmark < Sinatra::Base
 
   ENV['RACK_ENV'] ||= 'development'
@@ -11,6 +11,10 @@ class Bookmark < Sinatra::Base
 
   configure :development do
     DataMapper.setup(:default, "postgres://localhost/bookmark_manager_development")
+  end
+
+  configure :production do
+    DataMapper.setup(:default, ENV['DATABASE_URL'])
   end
 
   DataMapper.finalize
